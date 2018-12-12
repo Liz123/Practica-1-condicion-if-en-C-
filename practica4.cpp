756b9c85953b97b418f333ce5c80087bea3b8851
Liz123 <joanaReyna4@gmail.com>
2018-12-12 02:45:23 -0600

    Add files via upload

---
+++ practica4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+
+ int num1, num2;
+ cout <<"INGRESE EL NUMERO 1";
+ cin >>num1; 
+ cout <<"INGRESE EL NUMERO 2";
+ cin >>num2;
+if(num1> num2)
+{
+	cout<<"el numero 1 es mayor";
+	cout<<num1;
+}else {
+	cout<<"el numero 2 es mayor";
+	cout<<num2;
+}
+cin.get();
+cin.get();
+
+}
+
